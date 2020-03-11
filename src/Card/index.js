b53cfa1cdec0b184105eb100cb6755a565374e08
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2020-03-10 23:47:34 -0400

    Refactored Card and Footer

--- src/Card/index.js
+++ src/Card/index.js
@@ -2,14 +2,15 @@ import React, { useState } from 'react'
 
 import './card.css'
 
+
 const Card = (props) => {
   const [cardFace, changeCardFace] = useState(false)
   return(
-      <div className="scene scene--card">
+      <div className="scene">
         <div className={cardFace === false ? "card" : "card is-flipped"} 
              key={props.index}  
              onClick={() => changeCardFace(!cardFace)}>
-            <div className="card__face card__face--front">front</div>
+            <div className="card__face card__face--front">Front</div>
             <div className="card__face card__face--back">back</div> 
         </div>
       </div>
