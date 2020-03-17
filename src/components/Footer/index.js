9fe0de291bf4276c78a8774d2dd919b337ca90a0
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2020-03-17 03:29:40 -0500

    Testing theme

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,9 +5,9 @@ const Footer = (props) => {
     return(
         <footer className="footer">
             <h3 onClick={props.changePage.bind(null, 'First')}>FIRST</h3>
-            <h3 onClick={props.changePage.bind(null, '&larr')}>&larr;</h3>
+            <h3 id="icon" onClick={props.changePage.bind(null, '&larr')}>&larr;</h3>
             <h2>{props.feedPage}</h2>
-            <h3 onClick={props.changePage.bind(null, '&rarr')}>&rarr;</h3>
+            <h3 id="icon" onClick={props.changePage.bind(null, '&rarr')}>&rarr;</h3>
             <h3 onClick={props.changePage.bind(null, 'Last')}>LAST</h3>
         </footer>
     )
