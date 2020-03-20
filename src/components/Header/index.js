b01f69d119b558750de304dbeb7c96807bdbf795
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2020-03-20 12:55:20 -0400

    Menu screen now slides out

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 
 import { Icon } from 'react-icons-kit'
-import {menu} from 'react-icons-kit/feather/menu'
+import { menu, x } from 'react-icons-kit/feather/'
+import Menu from 'components/Menu'
 
 import './header.css'
 
+// let icon = menu
+// let showMenu = false
+// // const [showMenu, toggle] = useState(false)
+// const toggle = () => {
+//     showMenu = !showMenu
+//     icon = icon !== x ? x : menu
+//     console.log(icon)  
+// } 
 
 const Header = () => {
+    let hamburger = menu
+    let collapsed = x
+    const [showMenu, setMenu] = useState(false)
+    const [showHamburger, setIcon] = useState(true)
+    const toggle = (currentIcon) => {
+        setIcon(!showHamburger)
+        setMenu(!showMenu)
+        //icon = icon !== x ? x : menu 
+    }  
     return(
-        <header className="header testing">
+        <header className="header">
             <div className="nav-container">
-                <div className="nav-item logo">
+                <div className="nav-item logo" onClick={toggle}>
                     <div style={{
                         backgroundImage: 'url(/assets/syfy_logo_small.svg)',
                         height: '2.8em', width: '2.2em',
@@ -23,11 +41,13 @@ const Header = () => {
                 <div className="nav-item heading">
                     <h3 >Feed</h3>  
                 </div>
-                <div className="nav-item menu">
-                    <Icon icon={menu} size='1.4em' id="icon"  />
+                <div className="nav-item menu" 
+                     onClick={toggle}
+                >
+                    <Icon icon={showHamburger ? menu : x} size='1.4em' id="icon"  />
                 </div>
+                {showMenu ? <Menu /> : null}
             </div>
-            
         </header>
     )
 } 
---
+++ src/components/Menu/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './menu.css'
+
+
+const Menu = () => {
+    return(
+        <div className="menu-slideout">
+        </div>
+    )
+}
+
+export default Menu
