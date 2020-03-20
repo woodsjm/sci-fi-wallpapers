fb9f1706f5189a2d0f02daac3d4872f543d8f834
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2020-03-20 13:06:41 -0400

    Refactored Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,29 +6,14 @@ import Menu from 'components/Menu'
 
 import './header.css'
 
-// let icon = menu
-// let showMenu = false
-// // const [showMenu, toggle] = useState(false)
-// const toggle = () => {
-//     showMenu = !showMenu
-//     icon = icon !== x ? x : menu
-//     console.log(icon)  
-// } 
 
 const Header = () => {
-    let hamburger = menu
-    let collapsed = x
-    const [showMenu, setMenu] = useState(false)
-    const [showHamburger, setIcon] = useState(true)
-    const toggle = (currentIcon) => {
-        setIcon(!showHamburger)
-        setMenu(!showMenu)
-        //icon = icon !== x ? x : menu 
-    }  
+    let hamburger = menu, collapsed = x
+    const [showMenu, setMenu] = useState(false) 
     return(
         <header className="header">
             <div className="nav-container">
-                <div className="nav-item logo" onClick={toggle}>
+                <div className="nav-item logo" >
                     <div style={{
                         backgroundImage: 'url(/assets/syfy_logo_small.svg)',
                         height: '2.8em', width: '2.2em',
@@ -42,9 +27,12 @@ const Header = () => {
                     <h3 >Feed</h3>  
                 </div>
                 <div className="nav-item menu" 
-                     onClick={toggle}
+                     onClick={() => setMenu(!showMenu)}
                 >
-                    <Icon icon={showHamburger ? menu : x} size='1.4em' id="icon"  />
+                    <Icon 
+                        size='1.4em' id="icon"
+                        icon={showMenu ? collapsed : hamburger}  
+                    />
                 </div>
                 {showMenu ? <Menu /> : null}
             </div>
