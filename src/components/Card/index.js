9a21311f63a86ac789e7ab51ff3e209d05c6d5c9
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2020-03-23 15:27:14 -0400

    Fixed responsive layout issues

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -9,22 +9,22 @@ const album = 'https://res.cloudinary.com/dlwxbby8o/image/upload'
 const Card = ({index, publicId}) => {
   const [cardFace, changeCardFace] = useState(false)
   return(
-      <div className="scene">
-        <div className={cardFace === false ? "card" : "card is-flipped"} 
-             onClick={() => changeCardFace(!cardFace)}
-             key={index} 
-        >
-          <div className="card__face card__face--front" 
-               style={{
-                backgroundImage: `url("${album}/w_auto,c_scale/v1/${publicId}")`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: '100% 100%'}} 
-          > 
-          </div>
-            <div className="card__face card__face--back">back</div> 
+    <div className="scene">
+      <div className={cardFace === false ? "card" : "card is-flipped"} 
+           onClick={() => changeCardFace(!cardFace)}
+           key={index} 
+      >
+        <div className="card__face card__face--front" 
+             style={{
+              backgroundImage: `url("${album}/w_auto,c_scale/v1/${publicId}")`,
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: '100% 100%'}} 
+        > 
         </div>
+          <div className="card__face card__face--back">back</div> 
       </div>
-    )
+    </div>
+  )
 }
 
 export default Card
