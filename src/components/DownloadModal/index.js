85e47773eeb384edf8b8fa556931f55589025b34
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2020-04-16 18:11:38 -0500

    Fixed styling on first download option

--- src/components/DownloadModal/index.js
+++ src/components/DownloadModal/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Button, Header, Icon, Image, Modal, Menu, Dropdown } from 'semantic-ui-react'
+import { Button, Header, Icon, Modal, Menu, Dropdown } from 'semantic-ui-react'
+import { Image, Transformation } from 'cloudinary-react'
 
 import './downloadModal.css'
 
@@ -10,9 +11,11 @@ class DownloadModal extends React.Component {
       this.state = {
         masterpieces: [
           {key: 1, text: "American Gothic", value: "american_gothic", onClick: (e) => this.handleChange(e, 0)},
-          {key: 2, text: "Mona Lisa", value: "mona_lisa", onClick: (e) => this.handleChange(e, 1)}
+          {key: 2, text: "Mona Lisa", value: "mona_lisa", onClick: (e) => this.handleChange(e, 1)},
+          {key: 3, text: "Starry Night", value: "starry_night", onClick: (e) => this.handleChange(e, 2)},
+          {key: 4, text: "Guernica", value: "guernica", onClick: (e) => this.handleChange(e, 3)}
         ],
-        selectedMasterpiece: 'american_gothic'
+        selectedMasterpiece: 0
       }
       this.handleChange = this.handleChange.bind(this)
   }
@@ -22,13 +25,19 @@ class DownloadModal extends React.Component {
   }
 
   handleChange = (e, key) => {
-    console.log(this.state.masterpieces[key].value)
-    this.setState({ selectedMasterpiece: this.state.masterpieces[key].value})
+    console.log(key)
+    //console.log(this.state.masterpieces[key].value)
+    this.setState({ selectedMasterpiece: key})
   }
 
   render() {
     const { closeModal, showModal} = this.props
-    const baseUrl = 'https://res.cloudinary.com/dlwxbby8o/image/upload/w_auto,c_scale/v1586805907'
+    const baseUrl = 'https://res.cloudinary.com/dlwxbby8o/image/upload/w_auto,c_scale/v1_1/masterpieces'
+
+    let currentMasterpiece;
+    if (this.state.selectedMasterpiece && this.state.masterpieces) {
+      currentMasterpiece = this.state.masterpieces[this.state.selectedMasterpiece].text
+    }
 
     return(
       <Modal className="modal-container" open={showModal}>
@@ -36,27 +45,27 @@ class DownloadModal extends React.Component {
         <Modal.Content scrolling className="modal-content">
           <Modal.Description>
             <section className="modal-description-section">
-              <h5>Placeholder for list of image alteration options</h5>
+              <h5>Choose a masterpiece to style the wallpaper with</h5>
               <section className="selection-item">
                 <div>
-                  <div className="selection-item-box" id="selection-dropdown">
-                    <Menu compact>
-                    <Dropdown text="Masterpiece" options={this.state.masterpieces} simple item/>
-                    </Menu>
+                  <div className="selection-item-box">
+                    <div>
+                      <h6 className="selection-header">Masterpieces:</h6>
+                      <Menu id="selection-dropdown">
+                      <Dropdown text={currentMasterpiece} options={this.state.masterpieces} item/>
+                      </Menu>
+                    </div>
                   </div>
                 </div>
-                <div className="selection-item-box" style={{
-                  width: '100px',
-                  backgroundImage: `url("${baseUrl}/${this.state.selectedMasterpiece}")`,
-                  backgroundRepeat: 'no-repeat',
-                  backgroundSize: '100% 100%'}}>
+                <div className="selection-item-box" >
+                  <Image publicId={`masterpieces/${this.state.masterpieces[this.state.selectedMasterpiece].value}`} >
+                    <Transformation width="100" height="100" crop="scale" />
+                  </Image>
                 </div>
               </section>
             </section>
 
-           <p>In as name to here them deny wise this. As rapid woody my he me which. Men but they fail shew just wish next put. Led all visitor musical calling nor her. Within coming figure sex things are. Pretended concluded did repulsive education smallness yet yet described. Had country man his pressed shewing. No gate dare rose he. Eyes year if miss he as upon.</p>
-
-           <p>In as name to here them deny wise this. As rapid woody my he me which. Men but they fail shew just wish next put. Led all visitor musical calling nor her. Within coming figure sex things are. Pretended concluded did repulsive education smallness yet yet described. Had country man his pressed shewing. No gate dare rose he. Eyes year if miss he as upon.</p>
+           <p>New options coming soon.</p>
           </Modal.Description>
         </Modal.Content>
         <Modal.Actions >
