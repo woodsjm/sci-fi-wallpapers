a28a3a5f18b4e0dc5e9d35137e49f921ce14cce5
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2020-04-23 11:10:44 -0500

    Fixed layout of slideout menu items

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,37 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import { helpCircle, grid, home, info, user } from 'react-icons-kit/feather/'
+import { grid, info} from 'react-icons-kit/feather/'
 import { Icon } from 'react-icons-kit'
 
 import './menu.css'
 
 
 const routes = [
-    ["Feed", '/', grid], 
+    ["Wallpapers", '/', grid], 
     ["Info", '/about', info] 
 ]
 
 const Menu = ({ isVisible }) => {
     const menuItems = routes.map((route, idx) => {
         return(
-            <li key={idx} className="menu-item" >
-                <Icon id="nav-icon" icon={route[2]} size='1em'/>
+            <div className="menu-grid-row">
+                <div className="menu-icon-container">
+                    <Icon id="nav-icon" icon={route[2]} size='1em'/>
+                </div>
                 <Link to={route[1]}>
-                    {route[0]}
+                    <div className="menu-item-container">
+                        <p className="menu-item-text">{route[0]}</p>
+                    </div>
                 </Link>
-            </li>
+            </div>
         )
     })
 
     return(
-        <div id="menu-slideout"  style={{width: `${isVisible ? '50%' : '0'}`}} >
-            <section className="menu-container">
-                <div className="menu-list">
+        <section id="menu-slideout"  style={{width: `${isVisible ? '50%' : '0'}`}} >
+            <div className="menu-container">
+                <div className="menu-grid">
                     {menuItems}
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     )
 }
 
